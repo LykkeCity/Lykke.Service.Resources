c5b397c3819aeb04e6bca39d20a795ae225d33ba
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-04-12 19:01:03 +0300

    changes releated to PR

--- client/Lykke.Service.Resources.Client/ResourcesServiceClientSettings.cs
+++ client/Lykke.Service.Resources.Client/ResourcesServiceClientSettings.cs
@@ -1,9 +1,10 @@
-using System;
+using Lykke.SettingsReader.Attributes;
 
 namespace Lykke.Service.Resources.Client 
 {
     public class ResourcesServiceClientSettings 
     {
+        [HttpCheck("/api/isalive")]
         public string ServiceUrl {get; set;}
     }
 }
