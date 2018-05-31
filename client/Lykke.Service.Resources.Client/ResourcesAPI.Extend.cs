4821affaccab377ba7543c03ec373aa3e0fd84a4
troshkin sergey <legat.psu@gmail.com>
2018-05-31 20:53:18 +0500

    prevent memory leak

---
+++ client/Lykke.Service.Resources.Client/ResourcesAPI.Extend.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+// ReSharper disable once CheckNamespace
+namespace Lykke.Service.Resources.Client.AutorestClient
+{
+
+    public partial class ResourcesAPI
+    {
+        /// <summary>
+        /// Should be used to prevent memory leak in RetryPolicy
+        /// </summary>
+        public ResourcesAPI(Uri baseUri, HttpClient client) : base(client)
+        {
+            Initialize();
+            BaseUri = baseUri ?? throw new ArgumentNullException("baseUri");
+        }
+    }
+}
